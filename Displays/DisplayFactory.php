b06f4bd8d4187540d6bf896ea54b9910a61568eb
Angel Gonzalez <usftechacg@gmail.com>
2026-07-28 11:39:37 -0400

    Delete DisplayFactory.php

--- Displays/DisplayFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fabricate\Contracts\Displays;
-
-interface DisplayFactory
-{
-    public function embedded(string $driver): DisplayFactory;
-    public function windowed(string $driver): DisplayFactory;
-}
